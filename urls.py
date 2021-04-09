d90646715c5ede1deb817cc9bbf2cd0b621b9601
ronak123-rkd <53863357+ronak123-rkd@users.noreply.github.com>
2021-04-09 14:34:22 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-
-from .views import AuthViewSet
-
-router = routers.DefaultRouter(trailing_slash=False)
-router.register('api', AuthViewSet, basename='api')
-
-urlpatterns = router.urls
